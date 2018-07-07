0a8ac08ed7e894959d1278d734a61205fea9c7db
Lennart Jörelid <lj@jguru.se>
2018-07-07 12:36:52 +0200

    Added DaysOfWeek converter.

---
+++ shared-spi-jpa/src/main/kotlin/se/jguru/shared/spi/jpa/converter/DaysOfWeekAttributeConverter.kt
@@ -0,0 +1,77 @@
+/*-
+ * #%L
+ * Nazgul Project: jguru-shared-spi-jpa
+ * %%
+ * Copyright (C) 2018 jGuru Europe AB
+ * %%
+ * Licensed under the jGuru Europe AB license (the "License"), based
+ * on Apache License, Version 2.0; you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *       http://www.jguru.se/licenses/jguruCorporateSourceLicense-2.0.txt
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package se.jguru.shared.spi.jpa.converter
+
+import java.time.DayOfWeek
+import java.util.SortedSet
+import java.util.StringTokenizer
+import java.util.TreeSet
+import javax.persistence.AttributeConverter
+import javax.persistence.Converter
+import javax.xml.bind.annotation.XmlTransient
+
+/**
+ * Converter for [DayOfWeek] Sets, compacted into a comma-separated String
+ * representation, such as `1,2,4,5,6`
+ * 
+ * @author [Lennart J&ouml;relid](mailto:lj@jguru.se), jGuru Europe AB
+ */
+@XmlTransient
+@Converter(autoApply = true)
+open class DaysOfWeekAttributeConverter : AttributeConverter<SortedSet<DayOfWeek>, String> {
+
+    override fun convertToDatabaseColumn(attribute: SortedSet<DayOfWeek>?): String? = when (attribute) {
+
+        null -> null
+        else -> attribute.map { a -> "${a.value}" }.reduce { sum, current -> "$sum,$current" }
+    }
+
+    override fun convertToEntityAttribute(dbData: String?): SortedSet<DayOfWeek> {
+
+        val toReturn = TreeSet<DayOfWeek>()
+
+        if(dbData != null) {
+
+            val tokenizer = StringTokenizer(dbData, ",", false)
+            while(tokenizer.hasMoreTokens()) {
+
+                val currentToken = tokenizer.nextToken()
+
+                try {
+                    toReturn.add(DayOfWeek.of(Integer.parseInt(currentToken)));
+                } catch (e: Exception) {
+
+                    val permittedValues = DayOfWeek.values()
+                        .sorted()
+                        .map { a -> "${a.value}" }
+                        .reduce { acc, current -> "$acc,$current" }
+
+                    throw IllegalArgumentException("Could not interpret [$currentToken] as a DayOfWeek value. " +
+                        "Permitted values are: " + permittedValues, e)
+                }
+            }
+        }
+
+        // All Done.
+        return toReturn
+    }
+}
---
+++ shared-spi-jpa/src/test/kotlin/se/jguru/shared/spi/jpa/converter/DaysOfWeekAttributeConverterTest.kt
@@ -0,0 +1,79 @@
+package se.jguru.shared.spi.jpa.converter
+
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.time.DayOfWeek
+import java.util.SortedSet
+import java.util.TreeSet
+
+/**
+ *
+ * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
+ */
+class DaysOfWeekAttributeConverterTest {
+
+    // Shared state
+    private lateinit var weekDays: SortedSet<DayOfWeek>
+    private lateinit var weekEnds: SortedSet<DayOfWeek>
+    private val unitUnderTest = DaysOfWeekAttributeConverter()
+
+    @Before
+    fun setupSharedState() {
+
+        weekDays = TreeSet<DayOfWeek>(listOf(
+            DayOfWeek.MONDAY,
+            DayOfWeek.WEDNESDAY,
+            DayOfWeek.TUESDAY,
+            DayOfWeek.FRIDAY,
+            DayOfWeek.THURSDAY
+        ))
+
+        weekEnds = TreeSet<DayOfWeek>(listOf(
+            DayOfWeek.SATURDAY,
+            DayOfWeek.SUNDAY))
+    }
+
+    @Test
+    fun validateConvertingToTransportForm() {
+
+        // Assemble
+
+        // Act
+        val weekdayResults = unitUnderTest.convertToDatabaseColumn(weekDays)
+        val weekendResults = unitUnderTest.convertToDatabaseColumn(weekEnds)
+
+        // Assert
+        Assert.assertNull(unitUnderTest.convertToDatabaseColumn(null))
+        Assert.assertEquals("1,2,3,4,5", weekdayResults)
+        Assert.assertEquals("6,7", weekendResults)
+    }
+
+    @Test
+    fun validateConvertingFromTransportForm() {
+
+        // Assemble
+
+        // Act
+        val weekdays = unitUnderTest.convertToEntityAttribute("1,2,3,4,5")
+        val nothing = unitUnderTest.convertToEntityAttribute("")
+        val reallyNothing = unitUnderTest.convertToEntityAttribute(null)
+
+        // Assert
+        Assert.assertNotNull(reallyNothing)
+        Assert.assertNotNull(nothing)
+        Assert.assertTrue(nothing.isEmpty())
+        Assert.assertTrue(reallyNothing.isEmpty())
+
+        Assert.assertEquals(weekDays, weekdays)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun validateExceptionOnIncorrectWeekOfDayValue() {
+
+        // Act & Assert
+        val result = unitUnderTest.convertToEntityAttribute("1,2,42")
+
+        println("Got: $result")
+    }
+}
